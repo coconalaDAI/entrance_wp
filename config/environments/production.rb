437997bd1511c9a20171bd8d4767b2b5d13cc6e1
YutoSuzuki <15t4037x@vc.ibaraki.ac.jp>
2018-05-31 22:43:14 +0900

    ホストの修正v2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://entrancewp.herokuapp.com/users', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://entrancewp.herokuapp.com/'}
 
   config.action_mailer.smtp_settings = {
  :enable_starttls_auto => true,
