8f5824a7ef92606e0743a7c4627dc4b1e978ccc4
YutoSuzuki <15t4037x@vc.ibaraki.ac.jp>
2018-06-01 15:58:32 +0900

    仮登録メール送信後の画面作成

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,4 +1,8 @@
 class MypagesController < ApplicationController
   def index
   end
+
+  def after_sign_up
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'mypages/index',as:'mypages'
+  get 'mypages/after_sign_up'
 
-  devise_for :users
+  #devise_for :users
+  devise_for :users, controllers: {
+    :registrations => 'users/registrations'
+  }
   resources :question2s
   resources :question3s
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
