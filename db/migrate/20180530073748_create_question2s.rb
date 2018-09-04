f0cc26d88cce924310ac404e7e05ce368e84245b
YutoSuzuki <m1498212472@dea-love.net>
2018-09-04 19:42:10 +0900

    migrationfileの変更

--- db/migrate/20180530073748_create_question2s.rb
+++ db/migrate/20180530073748_create_question2s.rb
@@ -4,13 +4,13 @@ class CreateQuestion2s < ActiveRecord::Migration[5.1]
       t.string :phone_number
       t.string :postal_code
       t.string :address
-      t.integer :prefecture
+      t.string :prefecture
       t.integer :age
-      t.integer :gender
+      t.string :gender
       t.string :car_type
       t.string :model_year
       t.string :color
-      t.integer :licence_type
+      t.string :licence_type
       t.integer :user_id
 
       t.timestamps
